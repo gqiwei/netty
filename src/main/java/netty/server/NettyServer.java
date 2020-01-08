f5965485f4151771a5d69ac9b12f234ab371e113
郭奇卫 <757961559@qq.com>
2020-01-08 09:51:34 +0800

    项目创建

---
+++ src/main/java/netty/server/NettyServer.java
@@ -0,0 +1,16 @@
+package netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+
+/**
+ * @author gqw
+ * @mail gqiwei163@.com
+ * @description
+ * @date 2020-01-08
+ */
+public class NettyServer {
+    public static void main(String[] args) {
+        ServerBootstrap bossGroup = new ServerBootstrap();
+
+    }
+}
